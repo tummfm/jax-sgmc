18d3a71c7c45350f09a4e71be3bf2ca45ad9c163
Paul <paul.fuchs@tum.de>
2021-05-10 15:43:50 +0200

    Initial package

---
+++ setup.py
@@ -0,0 +1,32 @@
+import io
+import os
+import setuptools
+
+INSTALL_REQUIRES = [
+    'numpy',
+    'jax>=0.1.73',
+    'jaxlib>=0.1.52',
+    'dataclasses'
+]
+
+setuptools.setup(
+    name='jax_sgmc',
+    version='0.0.1',
+    license='Apache 2.0',
+    author='MMFM',
+    install_requires=INSTALL_REQUIRES,
+    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    description='Stochastic Gradient Monte Carlo',
+    python_requires='>=2.7',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+    ])

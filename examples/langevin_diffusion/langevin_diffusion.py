fffdf9645b28356a2f2cf95400b7ed8f4068ef96
Paul <paul.fuchs@tum.de>
2021-06-14 12:19:38 +0200

    Added example for SGLD with RMSprop and no adaption

--- examples/langevin_diffusion/langevin_diffusion.py
+++ examples/langevin_diffusion/langevin_diffusion.py
@@ -1,177 +1,259 @@
-
 import numpy as onp
-import time
 
 import jax
 import jax.numpy as jnp
+from jax import random
+from jax.scipy.stats import norm
 
-from jax import random, vmap
+from numpyro import sample as npy_smpl
+import numpyro.infer as npy_inf
+import numpyro.distributions as npy_dist
 
-import tensorflow
+from scipy.stats import gaussian_kde
 
 import matplotlib.pyplot as plt
 
-import numpyro
-
-from jax.scipy.stats import norm
-from jax.scipy.stats import uniform
-
-from functools import partial
-
-from jax_sgmc import scheduler
+from jax_sgmc import adaption
 from jax_sgmc import potential
-from jax_sgmc import integrator
 from jax_sgmc import data
+from jax_sgmc import scheduler
+from jax_sgmc import integrator
 
-#%% md
-
-# Todo: This is not the correct form to handle batches? -> It is possible but it
-# is kind of multiple batching
+################################################################################
+#
+# Reference Data
+#
+################################################################################
 
 N = 4
-M = 100
-
-samples = 1000
-
+samples = 1000  # Total samples
 sigma = 0.5
 
 key = random.PRNGKey(0)
-split, key = random.split(key)
+split1, split2, split3 = random.split(key, 3)
 
-w = random.uniform(split, minval=-1, maxval=1, shape=(N, 1))
-
-def draw_samples(key, w):
-    # For each sample draw a new set of predictor variables x and calculate
-    # the response
-
-    split1, split2, key = random.split(key, 3)
-
-    noise = jnp.sqrt(sigma) * random.normal(split2, shape=(samples, 1))
-    x = random.uniform(split1, minval=-10, maxval=10, shape=(samples, N))
-    x = jnp.stack([x[:, 0] + x[:, 1], x[:, 1], 0.1 * x[:, 2] - 0.5 * x[:, 3],
-                   x[:, 3]]).transpose()
-
-    y = jnp.matmul(x, w) + noise
-
-    return x, y
-
-# Bring in some correlations
-
-X, Y = draw_samples(key, w)
+w = random.uniform(split3, minval=-1, maxval=1, shape=(N, 1))
+noise = jnp.sqrt(sigma) * random.normal(split2, shape=(samples, 1))
+x = random.uniform(split1, minval=-10, maxval=10, shape=(samples, N))
+x = jnp.stack([x[:, 0] + x[:, 1], x[:, 1], 0.1 * x[:, 2] - 0.5 * x[:, 3],
+               x[:, 3]]).transpose()
+y = jnp.matmul(x, w) + noise
 
 
-ds = tensorflow.data.Dataset.from_tensor_slices({"x": X, "y": Y})
+################################################################################
+#
+# Solution with numpyro
+#
+################################################################################
 
-data_loader = data.NumpyDataLoader(M, x=X, y=Y)
-# data_loader = data.TensorflowDataLoader(ds, mini_batch_size=M, shuffle_cache=1000)
-
-batch_fn = data.random_reference_data(data_loader, cached_batches_count=999)
-# First we need to define the (deterministic) model
-
-def model(sample, observations):
-    weights = sample["w"]
-    predictors = observations["x"]
-    return jnp.dot(predictors, weights)
-
-
-# The combination of prior and distribution form the posterior, from which we
-# sampling is performed.
-
-def likelihood(sample, observations):
-    sigma = sample["sigma"]
-    y = observations["y"]
-    y_pred = model(sample, observations)
-    return norm.logpdf(y - y_pred, scale=sigma)
-
-def prior(sample):
-    return 0.0
-
-
-# Initial sample
-test_w = random.normal(key, (N, 1))
-
-sample = {"w": test_w, "sigma": jnp.array(10.0)}
-# Potential strategy
-pot_fn = potential.minibatch_potential(prior=prior,
-                                      likelihood=likelihood,
-                                      strategy="vmap")
-
-# Integrator
-integ = integrator.langevin_diffusion(pot_fn, batch_fn)
-
-# Scheduler
-step_size = scheduler.polynomial_step_size_first_last(0.005, 0.00001)
-sched_fn = scheduler.init_scheduler(step_size)
-
-# Update function
-
-@jax.jit
-def update(state, it):
-    int_state, sched_state = state
-    schedule = sched_fn[2](sched_state)
-
-    int_state = integ[1](int_state, schedule)
-
-    sched_state = sched_fn[1](sched_state)
-    return (int_state, sched_state), (integ[2](int_state))
-
-# Inital state
-iterations = 10000
-sched_init = sched_fn[0](iterations)
-int_init = integ[0](sample)
-
-state = (int_init, sched_init)
-
-# for _ in range(iterations):
-#   state, _ = update(state, None)
-
-# Run
-
-start = time.time()
-
-@jax.jit
-def compute(state):
-  _, results = jax.lax.scan(update, state, jnp.arange(iterations))
-  return results
-
-init_state = (int_init, sched_init)
-
-print(jax.make_jaxpr(compute)(init_state))
-results = compute(init_state)
 
 def numpyro_model(y_obs=None):
-  sigma = numpyro.sample("sigma", numpyro.distributions.Uniform(low=0.0, high=10.0))
-  weights = numpyro.sample("weights", numpyro.distributions.Uniform(low=-10 * jnp.ones((N, 1)), high=10 * jnp.ones((N, 1))))
+  sigma = npy_smpl("sigma", npy_dist.Uniform(low=0.0, high=10.0))
+  weights = npy_smpl("weights",
+                     npy_dist.Uniform(low=-10 * jnp.ones((N, 1)),
+                                      high=10 * jnp.ones((N, 1))))
 
-  y_pred = jnp.matmul(X, weights)
-  numpyro.sample("likelihood", numpyro.distributions.Normal(loc=y_pred, scale=sigma), obs=y_obs)
+  y_pred = jnp.matmul(x, weights)
+  npy_smpl("likelihood", npy_dist.Normal(loc=y_pred, scale=sigma), obs=y_obs)
 
 
-kernel = numpyro.infer.HMC(numpyro_model)
-mcmc = numpyro.infer.MCMC(kernel, 1000, 1000)
-mcmc.run(random.PRNGKey(0), y_obs=Y)
+# Collect 1000 samples
+
+kernel = npy_inf.HMC(numpyro_model)
+mcmc = npy_inf.MCMC(kernel, 1000, 1000)
+mcmc.run(random.PRNGKey(0), y_obs=y)
 mcmc.print_summary()
 
-samples = mcmc.get_samples()
+w_npy = mcmc.get_samples()["weights"]
 
-print(w)
-print(results["w"][-1,:])
-print(results["sigma"][-10:])
-print(f"Results in {time.time() - start} seconds")
-# print(jax.make_jaxpr(update)((int_init, sched_init), 0.0))
+################################################################################
+#
+# Solution with Jax SGMC
+#
+################################################################################
 
-plt.plot(results["sigma"])
+
+M = 10
+cs = 1000
+
+data_loader = data.NumpyDataLoader(M, x=x, y=y)
+batch_fn = data.random_reference_data(data_loader, cached_batches_count=cs)
+
+
+# == Model definition ==========================================================
+
+def model(sample, observations):
+  weights = sample["w"]
+  predictors = observations["x"]
+  return jnp.dot(predictors, weights)
+
+
+def likelihood(sample, observations):
+  sigma = sample["sigma"]
+  y = observations["y"]
+  y_pred = model(sample, observations)
+  return norm.logpdf(y - y_pred, scale=sigma)
+
+
+def prior(unused_sample):
+  return 0.0
+
+
+# If the model is more complex, the strategy can be set to map for sequential
+# evaluation and pmap for parallel evaluation.
+potential_fn = potential.minibatch_potential(prior=prior,
+                                             likelihood=likelihood,
+                                             strategy="vmap")
+
+# == Solver Setup ==============================================================
+
+# Number of iterations
+iterations = 10000
+
+# Adaption strategy
+rms_prop = adaption.rms_prop()
+
+# Integrators
+default_integrator = integrator.langevin_diffusion(potential_fn,
+                                                   batch_fn)
+rms_integrator = integrator.langevin_diffusion(potential_fn,
+                                               batch_fn,
+                                               rms_prop)
+
+# Initial value for starting
+sample = {"w": jnp.zeros((N, 1)), "sigma": jnp.array(10.0)}
+
+# Schedulers
+default_step_size = scheduler.polynomial_step_size_first_last(first=0.001,
+                                                              last=0.000005)
+rms_step_size = scheduler.polynomial_step_size_first_last(first=0.05,
+                                                          last=0.001)
+default_scheduler = scheduler.init_scheduler(default_step_size)
+rms_scheduler = scheduler.init_scheduler(rms_step_size)
+
+# Initial states
+default_state = (
+default_integrator[0](sample), default_scheduler[0](iterations))
+rms_state = (rms_integrator[0](sample), rms_scheduler[0](iterations))
+
+
+# Update functions
+@jax.jit
+def default_update(state, _):
+  integrator_state, scheduler_state = state
+
+  # Get the schedule for the next step
+  schedule = default_scheduler[2](scheduler_state)
+
+  # Integrate
+  integrator_state = default_integrator[1](integrator_state, schedule)
+
+  # Update the scheduler
+  scheduler_state = default_scheduler[1](scheduler_state)
+
+  return (integrator_state, scheduler_state), default_integrator[2](
+    integrator_state)
+
+
+@jax.jit
+def rms_update(state, _):
+  integrator_state, scheduler_state = state
+
+  # Get the schedule for the next step
+  schedule = rms_scheduler[2](scheduler_state)
+
+  # Integrate
+  integrator_state = rms_integrator[1](integrator_state, schedule)
+
+  # Update the scheduler
+  scheduler_state = rms_scheduler[1](scheduler_state)
+
+  return (integrator_state, scheduler_state), rms_integrator[2](
+    integrator_state)
+
+
+_, default = jax.lax.scan(default_update, default_state, jnp.arange(iterations))
+_, rms = jax.lax.scan(rms_update, rms_state, jnp.arange(iterations))
+
+################################################################################
+#
+# Results
+#
+################################################################################
+
 
 plt.figure()
-plt.title("w0 vs w1")
+plt.title("Sigma")
 
-plt.scatter(results["w"][-4000:, 0], results["w"][-4000:, 1])
-plt.plot(samples["weights"][:, 0], samples["weights"][:, 1], "r")
+plt.plot(default["sigma"], label="Default")
+plt.plot(rms["sigma"], label="RMSprop")
+
+plt.legend()
+
+# Now, only take the last 4000 steps
+
+w_default = default["w"][-4000:, :]
+w_rms = rms["w"][-4000:, :]
+
+# Contours of numpyro solution
+
+levels = onp.linspace(0.1, 1.0, 5)
+
+# w1 vs w2
+w12 = gaussian_kde(jnp.squeeze(w_npy[:, 0:2].transpose()))
+w1d = onp.linspace(0.00, 0.20, 100)
+w2d = onp.linspace(-0.70, -0.30, 100)
+W1d, W2d = onp.meshgrid(w1d, w2d)
+p12d = onp.vstack([W1d.ravel(), W2d.ravel()])
+Z12d = onp.reshape(w12(p12d).T, W1d.shape)
+Z12d /= Z12d.max()
+
+# Default
 
 plt.figure()
-plt.title("w2 vs w3")
+plt.title("w_1 vs w_2 (default)")
 
-plt.scatter(results["w"][-4000:, 2], results["w"][-4000:, 3])
-plt.plot(samples["weights"][:, 2], samples["weights"][:, 3], "r")
+plt.contour(W1d, W2d, Z12d, levels, colors='red', linewidths=0.5)
+plt.plot(w_default[:, 0], w_default[:, 1], 'o', alpha=0.5, markersize=0.5,
+         zorder=-1)
+
+# rm
+
+
+plt.figure()
+plt.title("w_1 vs w_2 (rms)")
+
+plt.xlim([0.07, 0.12])
+plt.ylim([-0.525, -0.450])
+plt.contour(W1d, W2d, Z12d, levels, colors='red', linewidths=0.5)
+plt.plot(w_rms[:, 0], w_rms[:, 1], 'o', alpha=0.5, markersize=0.5, zorder=-1)
+
+# w3 vs w4
+
+w34 = gaussian_kde(jnp.squeeze(w_npy[:, 2:4].transpose()))
+w3d = onp.linspace(-0.3, -0.05, 100)
+w4d = onp.linspace(-0.75, -0.575, 100)
+W3d, W4d = onp.meshgrid(w3d, w4d)
+p34d = onp.vstack([W3d.ravel(), W4d.ravel()])
+Z34d = onp.reshape(w34(p34d).T, W3d.shape)
+Z34d /= Z34d.max()
+
+# Default
+
+plt.figure()
+
+plt.title("w_3 vs w_4 (default)")
+
+plt.contour(W3d, W4d, Z34d, levels, colors='red', linewidths=0.5)
+plt.plot(w_default[:, 2], w_default[:, 3], 'o', alpha=0.5, markersize=0.5,
+         zorder=-1)
+
+# rm
+
+
+plt.figure()
+plt.title("w_3 vs w_4 (rms)")
+plt.contour(W3d, W4d, Z34d, levels, colors='red', linewidths=0.5)
+plt.plot(w_rms[:, 2], w_rms[:, 3], 'o', alpha=0.5, markersize=0.5, zorder=-1)
 
 plt.show()

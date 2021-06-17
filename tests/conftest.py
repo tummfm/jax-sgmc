2d213180825c70e066715c26f1d262e82e1fadb9
Paul <paul.fuchs@tum.de>
2021-06-17 12:33:06 +0200

    Changed xla forcing multiple devices

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,32 @@ from collections import  namedtuple
 
 import pytest
 
-from jax_sgmc import util
+import os
+from jax.lib import xla_bridge
 
-pmap_setup_state = namedtuple("pmap_setup_state",
-                              ["host_count"])
+from jax import test_util
 
-# # Setup multi-device environment
-# @pytest.fixture(scope='session')
-# def pmap_setup():
-#   hc = 5 # Host count
-#   util.set_host_device_count(hc)
-#   return pmap_setup_state(hc)
+# Setup multi-device environment
+@pytest.fixture(scope='session', autouse=True)
+def pmap_setup():
+  # Setup
+  prev_xla_flags = os.getenv("XLA_FLAGS")
+  flags_str = prev_xla_flags or ""
+  # Don't override user-specified device count, or other XLA flags.
+  if "xla_force_host_platform_device_count" not in flags_str:
+    os.environ["XLA_FLAGS"] = (flags_str +
+                               " --xla_force_host_platform_device_count=12")
+  # Clear any cached backends so new CPU backend will pick up the env var.
+  xla_bridge.get_backend.cache_clear()
+
+  # Run
+  yield
+
+  # Reset to previous configuration in case other test modules will be run.
+  if prev_xla_flags is None:
+    del os.environ["XLA_FLAGS"]
+  else:
+    os.environ["XLA_FLAGS"] = prev_xla_flags
+  xla_bridge.get_backend.cache_clear()
+
+

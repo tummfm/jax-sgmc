6cf166a9a9965543f305c2987ed22455ea288b6f
Paul <paul.fuchs@tum.de>
2021-06-18 09:43:50 +0200

    Solver sequential chain evaluation

--- jax_sgmc/solver.py
+++ jax_sgmc/solver.py
@@ -1,15 +1,26 @@
+# Copyright 2021 Multiscale Modeling of Fluid Materials, TU Munich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Solvers for Stochastic Gradient Bayesian Inference."""
 
-from typing import Callable, Any, Optional, Tuple
+from typing import Callable, Any, Tuple
 
-from jax import lax
+from jax import lax, jit
 import jax.numpy as jnp
+from jax_sgmc import io
 
 PyTree = Any
-
-from jax_sgmc import io
-from jax_sgmc.util import host_callback
-
 
 # Todo: Implement kernel
 # Todo: Implement chain runner
@@ -18,14 +29,17 @@ from jax_sgmc.util import host_callback
 def mcmc(solver,
          scheduler,
          strategy="map",
-         init_strategy="sample",
          saving=None):
-  """Extends the solver  .
+  """Runs the solver for multiple chains and saves the collected samples
 
   Args:
     solver: Computes the next state form a given state.
     scheduler: Schedules solver parameters such as temperature and burn in.
+    strategy: Run multiple chains in parallel or vectorized
+    saving: Save samples via host_callback (if saving requires much memory)
 
+  Returns:
+    Returns function which runs the solver for a given number of iterations.
 
   """
 
@@ -36,34 +50,58 @@ def mcmc(solver,
   scheduler_init, scheduler_next, scheduler_get = scheduler
   _, solver_update, solver_get = solver
 
+  @jit
   def _update(state, _):
     solver_state, scheduler_state = state
     schedule = scheduler_get(scheduler_state)
-    new_state = solver_update(solver_state, schedule)
+    new_state, stats = solver_update(solver_state, schedule)
     saved = save(schedule, solver_get(new_state))
-    scheduler_state = scheduler_next(scheduler_state)
+    # Update the scheduler with additional information from the integrator, such
+    # as the acceptance ratio of AMAGOLD
+    if stats is not None:
+      scheduler_state = scheduler_next(scheduler_state, **stats)
+    else:
+      scheduler_state = scheduler_next(scheduler_state)
     return (new_state, scheduler_state), saved
 
+  # Scan over the update function. Postprocessing the samples is required if
+  # saving is None to sort out not accepted samples. Samples are not accepted
+  # due tu burn in or thinning.
   def run(*states, iterations: int = 1e5):
-    if len(states) == 1:
-      # Initializes the scheduler and run for the number of iterations
-      scheduler_state = scheduler_init(iterations)
-      _, saved = lax.scan(_update,
-                          (states[0], scheduler_state),
-                          jnp.arange(iterations))
-      return postprocess_saving(saved)
+    if strategy == "map":
+      def run_single():
+        for state in states:
+          init_saving()
+          scheduler_state = scheduler_init(iterations)
+          _, saved = lax.scan(_update,
+                              (state, scheduler_state),
+                              jnp.arange(iterations))
+          yield postprocess_saving(saved)
+      chains = list(run_single())
     else:
-      raise NotImplementedError("Currently only a single chain can be "
-                                "evaluated in parallel.")
-
+      raise NotImplementedError("Parallel execution is currently not "
+                                "supported")
+    # No need to return a list for a single chain result
+    if len(chains) == 1:
+      return chains.pop()
+    else:
+      return chains
   return run
 
 
-def sgld(integrator) -> Tuple[Callable, Callable, Callable]:
+def sgmc(integrator) -> Tuple[Callable, Callable, Callable]:
   """Initializes the standard SGLD - sampler.
 
   This sampler simply integrates without acceptance/rejection or parallel
   tempering.
+
+  Args:
+    integrator: sgld or leapfrog.
+
+  Returns:
+    Returns a solver which depends only on external variables from the
+    scheduler, such as the step size.
+
   """
 
   init_integrator, update_integrator, get_integrator = integrator
@@ -72,7 +110,8 @@ def sgld(integrator) -> Tuple[Callable, Callable, Callable]:
     return init_integrator(*args, **kwargs)
 
   def update(state, schedule):
-    return update_integrator(state, schedule)
+    # No statistics such as acceptance ratio
+    return update_integrator(state, schedule), None
 
   def get(state):
     return get_integrator(state)

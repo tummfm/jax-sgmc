40bbe841a8e93f322a3a2bf31dc37ef2c8a99150
Paul <paul.fuchs@tum.de>
2021-06-28 11:41:18 +0200

    Added rules to transform a pytree into a dict of dicts

--- jax_sgmc/io.py
+++ jax_sgmc/io.py
@@ -18,17 +18,19 @@
 #       itself.
 
 import abc
+import itertools
 
 from pathlib import Path
 from functools import partial
 import threading
 
-from typing import Any, NoReturn, Union, Tuple, Callable
+from typing import Any, NoReturn, Union, Tuple, Callable, Type, Dict
 
 from collections import namedtuple
 
 import numpy as onp
 
+import jax
 import jax.numpy as jnp
 from jax import tree_util, lax
 
@@ -42,11 +44,214 @@ try:
 except ModuleNotFoundError:
   h5py = None
 
+# Import haiku to register a rule for transforming FlatMapping into a dict
+try:
+  import haiku._src.data_structures as haiku_ds
+except ModuleNotFoundError:
+  haiku_ds = None
+
 from jax_sgmc import data
 from jax_sgmc import scheduler
 from jax_sgmc.util import host_callback
 
 PyTree = Any
+
+# Global rules for translating a tree-node into a dict
+_dictionize_rules: Dict[Type, Callable] = {}
+
+def _get_dictionize_rule():
+  global _dictionize_rules
+  return _dictionize_rules
+
+def register_dictionize_rule(type: Type) -> NoReturn:
+  """Decorator to define new rules transforming a pytree node to a dict.
+
+  By default, transformations are defined for some default types:
+    - list
+    - dict
+    - (named)tuple
+
+  Additionaly, transformation for the following optional libraries are
+  implemented:
+    - haiku._src.data_structures.FlatMapping
+
+  New a new transformation rule is a function, which accepts a pytree node of
+  a specific type an returns a iterable, which itself returns `(key, value)`-
+  pairs.
+
+  .. doctest::
+
+    >>> from jax_sgmc import io
+    >>> from jax.tree_util import register_pytree_node
+    >>>
+    >>> class SomeClass:
+    ...   def __init__(self, value):
+    ...     self._value = value
+    >>>
+    >>> # Do not forget to register the class as jax pytree node
+    >>> register_pytree_node(SomeClass,
+    ...                      lambda sc: (sc._value, None),
+    ...                      lambda _, data: SomeClass(value=data))
+    >>>
+    >>> # Now define a rule to transform the class into a dict
+    >>> @io.register_dictionize_rule(SomeClass)
+    ... def some_class_to_dict(instance_of_some_class):
+    ...   return [("this_is_the_key", instance_of_some_class._value)]
+    >>>
+    >>> some_class = SomeClass({'a': 0.0, 'b': 0.5})
+    >>> some_class_as_dict = io.pytree_to_dict(some_class)
+    >>>
+    >>> print(some_class_as_dict)
+    {'this_is_the_key': {'a': 0.0, 'b': 0.5}}
+
+  Args:
+    type: Type (or class) of the currently undefined node
+
+  Returns:
+    The decoreated function is not intended to be used directly.
+
+  """
+  def register_function(rule: Callable):
+    global _dictionize_rules
+    assert type not in _dictionize_rules.keys(), f"Rule for {type.__name__} is " \
+                                                f"already defined."
+    _dictionize_rules[type] = rule
+  return register_function
+
+def _dictionize(node):
+  """Apply the registered rules to a pytree node."""
+  global _dictionize_rules
+  # If there are no leaves, then a dictionize rule is not required
+  if len(tree_util.tree_leaves(node)) == 0:
+    return []
+  for node_type, node_rule in _dictionize_rules.items():
+    if isinstance(node, node_type):
+      return node_rule(node)
+  raise NotImplementedError(f"Node type {type(node).__name__} cannot be "
+                            f"transformed to dict, please register a rule.")
+
+def pytree_to_dict(tree: PyTree):
+  """Construct a dictionary from a pytree.
+
+  Transforms each node of a pytree to a dict by appling defined dictionize. New
+  rules can be specified by the :func:`jax_sgmc.io.register_dictionize_rule`
+  -decorator.
+
+  .. doctest::
+
+    >>> from jax_sgmc import io
+    >>> some_tree = {'a': 0.0, 'b': {'b1': [0.0, 0.1], 'b2': 0.0}}
+    >>> as_dict = io.pytree_to_dict(some_tree)
+    >>> print(as_dict)
+    {'a': 0.0, 'b': {'b1': {'list_element_0': 0.0, 'list_element_1': 0.1}, 'b2': 0.0}}
+
+  Args:
+    tree: All nodes of the tree must either have no children or a registered
+      transformation to dict.
+
+  Returns:
+    Returns the tree as a dict with similar structure.
+
+  """
+  global _dictionize_rules
+  pytree_def = tree_util.tree_structure(tree)
+  if tree_util.treedef_is_leaf(pytree_def):
+    return tree
+  else:
+    return {key: pytree_to_dict(childs)
+            for key, childs
+            in _dictionize(tree)}
+
+def pytree_dict_keys(tree: PyTree):
+  """Returns a list of keys to acces the leaves of the tree.
+
+  Args:
+    tree: Pytree as a dict
+
+  Returns:
+    Returns a list of tuples, where each tuple contains the keys to access the
+    leaf of the flattened pytree in the unflattened dict.
+
+    For example:
+
+    .. doctest::
+
+      >>> from jax import tree_leaves
+      >>> from jax_sgmc import io
+      >>> pytree = {"a": [0.0, 1.0], "b": 2.0}
+      >>> pytree_as_dict = io.pytree_to_dict(pytree)
+      >>> pytree_leaves = tree_leaves(pytree_as_dict)
+      >>> pytree_keys = io.pytree_dict_keys(pytree_as_dict)
+      >>> print(pytree_leaves)
+      [0.0, 1.0, 2.0]
+      >>> print(pytree_keys)
+      [('a', 'list_element_0'), ('a', 'list_element_1'), ('b',)]
+
+  """
+  node = pytree_to_dict(tree)
+  leaves, treedef = tree_util.tree_flatten(node)
+  idx_tree = tree_util.tree_unflatten(treedef, [idx for idx in range(len(leaves))])
+  key_list = [None] * len(leaves)
+  def _recurse(node, path):
+    if type(node) is int:
+      key_list[node] = path
+    else:
+      for key, value in node.items():
+        _recurse(value, path + [key])
+  _recurse(idx_tree, [])
+  return [tuple(key) for key in key_list]
+
+def dict_to_pytree(pytree_as_dict: dict, target: PyTree):
+  """Restores the original tree structure given by the target from a dict.
+
+  Restores the pytree as a dict to its original tree structure. This function
+  can also operate on subtrees, as long as the subtree (a dict) of the pytree
+  as dict matches the subtree of the target dict.
+
+  .. doctest::
+
+    >>> from jax_sgmc import io
+    >>> some_tree = {'a': 0.0, 'b': {'b1': [0.0, 0.1], 'b2': 0.0}}
+    >>> as_dict = io.pytree_to_dict(some_tree)
+    >>> sub_pytree = io.dict_to_pytree(as_dict['b'], some_tree['b'])
+    >>> print(sub_pytree)
+    {'b1': [0.0, 0.1], 'b2': 0.0}
+
+  Args:
+    pytree_as_dict: A pytree which has been transformed to a dict of dicts.
+    target: A pytree defining the original tree structure.
+
+  """
+  target_dict_keys = pytree_dict_keys(target)
+  tree_structure = tree_util.tree_structure(target)
+  def _recurse_get(key_list):
+    key_list = list(key_list)
+    element = pytree_as_dict
+    while(len(key_list) > 0):
+      element = element.get(key_list.pop(0))
+    return element
+  new_leaves = map(_recurse_get, target_dict_keys)
+  return tree_util.tree_unflatten(tree_structure, new_leaves)
+
+@register_dictionize_rule(dict)
+def _dict_to_dict(some_dict: dict):
+  return some_dict.items()
+
+@register_dictionize_rule(list)
+def _list_to_dict(some_list: list):
+  return ((f"list_element_{idx}", value) for idx, value in enumerate(some_list))
+
+@register_dictionize_rule(tuple)
+def _namedtuple_to_dict(some_tuple: Union[tuple, namedtuple]):
+  if hasattr(some_tuple, '_fields'):
+    return some_tuple._asdict().items()
+  else:
+    return ((f"list_element_{idx}", value) for idx, value in enumerate(some_tuple))
+
+if haiku_ds is not None:
+  @register_dictionize_rule(haiku_ds.FlatMapping)
+  def _flat_mapping_to_dict(flat_mapping: haiku_ds.FlatMapping):
+    return haiku_ds.to_immutable_dict(flat_mapping).items()
 
 saving_state = namedtuple("saving_state",
                           ["chain_id",
@@ -103,32 +308,6 @@ class DataCollector(metaclass=abc.ABCMeta):
   @abc.abstractmethod
   def resume(self):
     """Called to restore data loader state and return sample states. """
-
-
-class PickleCollector(DataCollector):
-  """Stores samples in pickle format. """
-
-  def __init__(self):
-    raise NotImplementedError("Not implemented yet")
-
-  def register_data_loader(self, data_loader: data.DataLoader):
-    """Register data loader to save the state. """
-
-  def register_chain(self) -> int:
-    """Register a chain to save samples from. """
-
-  def save(self, chain_id: int, values):
-    """Called with collected samples. """
-
-  def finalize(self, chain_id: int):
-    """Called after solver finished. """
-
-  def checkpoint(self, chain_id: int, state):
-    """Called every n'th step. """
-
-  def resume(self):
-    """Called to restore data loader state and return sample states. """
-
 
 class JSONCollector(DataCollector):
   """Save samples in json format.
@@ -204,29 +383,59 @@ class JSONCollector(DataCollector):
 
 
 class HDF5Collector(DataCollector):
+  """Save to hdf5 format.
 
-  def __init__(self):
+  Args:
+    file: hdf5 file object
+
+  """
+
+  def __init__(self, file=h5py.File):
     assert h5py is not None, "h5py must be installed to use this DataCollector."
     self._finished = []
-    self._samples = []
+    self._sample_count = []
+    self._leaf_names = []
+    self._file = file
 
   def register_data_loader(self, data_loader: data.DataLoader):
     """Register data loader to save the state. """
 
-  def register_chain(self) -> int:
+  def register_chain(self,
+                     init_sample: PyTree,
+                     init_checkpoint: PyTree,
+                     static_information: PyTree
+                     ) -> int:
     """Register a chain to save samples from. """
     chain_id = len(self._finished)
+    leaf_names = [f"/chain~{chain_id}{groupname}" for groupname in
+                  _groupnames(init_sample)]
+    leaves = tree_util.tree_leaves(init_sample)
+    # Build the datasets
+    # Todo: Maybe flatten completely
+    for leaf_name, leaf in zip(leaf_names, leaves):
+      new_shape = tuple(int(s) for s in
+                        itertools.chain([static_information.samples_collected],
+                                        leaf.shape))
+      chain = self._file.create_group(f"/chain~{chain_id}", track_order=True)
+      self._file.create_dataset(
+        leaf_name,
+        shape=new_shape,
+        dtype=leaf.dtype)
+      # Save the leaf names in creational order make rebuild of tree possible
+      chain.attrs.create(leaf_name, new_shape)
+    self._sample_count.append(0)
+    self._leaf_names.append(leaf_names)
     self._finished.append(threading.Barrier(2))
-    self._samples.append([])
     return chain_id
 
   def save(self, chain_id: int, values):
-    """Called with collected samples. """
-    self._samples[chain_id].append(
-      tree_util.tree_map(onp.array, values))
+    """Save new leaves to dataset. """
+    for leaf_name, value in zip(self._leaf_names[chain_id], values):
+      self._file[leaf_name][self._sample_count[chain_id]] = value
+    self._sample_count[chain_id] += 1
 
   def finalize(self, chain_id: int):
-    """Called after solver finished. """
+    """Wait for all writing to be finished. """
     # Is called after all host callback calls have been processed
     self._finished[chain_id].wait()
 
@@ -236,32 +445,9 @@ class HDF5Collector(DataCollector):
   def resume(self):
     """Called to restore data loader state and return sample states. """
 
-
-  @staticmethod
-  def _groupnames(tree):
-    def one_flatten_step(tree):
-      """Helper to flatten tree for one step if it is not a leaf."""
-      return lambda t: tree != t
-
-    def recurse(treedef, tree):
-      """Recursively build the node names. """
-      if tree_util.treedef_is_leaf(treedef):
-        for idx in range(treedef.num_leaves):
-          yield f"/{type(tree).__name__}~{idx}"
-      else:
-        # Get the child treedefs and the node treedef
-        children = [treedef.children(),
-                    tree_util.tree_flatten(
-                      tree, is_leaf=one_flatten_step(tree))[0]]
-        for idx, (child_treedef, child_tree) in enumerate(zip(*children)):
-          for child_name in recurse(child_treedef, child_tree):
-            yield f"/{type(tree).__name__}~{idx}" + child_name
-
-    treedef = tree_util.tree_structure(tree)
-    return list(recurse(treedef, tree))
-
   def finished(self, chain_id: int):
-    """Simply return, nothing to wait for. """
+    """Return after everything has been written to the file. """
+    self._finished[chain_id].wait()
     return None
 
 class MemoryCollector(DataCollector):
@@ -270,13 +456,19 @@ class MemoryCollector(DataCollector):
   The RAM is usually larger than the device array and thus allows to store a
   greater number of samples.
 
+  Args:
+    save_dir: Directory to output results as numpy-npz with one file per chain.
+      If none, the results will only be returned.
+
   """
 
-  def __init__(self):
+  def __init__(self, save_dir=None):
     self._finished = []
     self._samples = []
     self._samples_count = []
     self._treedefs = []
+    self._leafnames = []
+    self._dir = save_dir
 
   def register_data_loader(self, data_loader: data.DataLoader):
     """Register data loader to save the state. """
@@ -295,10 +487,12 @@ class MemoryCollector(DataCollector):
       new_shape = tuple(int(s) for s in new_shape)
       return new_shape
     sample_cache = [onp.zeros(leaf_shape(leaf), dtype=leaf.dtype) for leaf in leaves]
+
     self._finished.append(threading.Barrier(2))
     self._samples.append(sample_cache)
     self._treedefs.append(treedef)
     self._samples_count.append(0)
+    self._leafnames.append(_groupnames(init_sample))
     return chain_id
 
   def save(self, chain_id: int, values):
@@ -312,6 +506,14 @@ class MemoryCollector(DataCollector):
     """Called after solver finished. """
     # Is called after all host callback calls have been processed
     self._finished[chain_id].wait()
+    output_dir = Path(self._dir)
+    output_file = output_dir / f"chain_{chain_id}.npz"
+    output_dir.mkdir(exist_ok=True)
+    onp.savez(
+      output_file,
+      **{name: ary for name, ary in zip(self._leafnames[chain_id],
+                                        self._samples[chain_id])
+         })
 
   def finished(self, chain_id):
     self._finished[chain_id].wait()
@@ -514,3 +716,26 @@ def no_save() -> Saving:
             "samples": state.data}
 
   return init, save, postprocess
+
+# Todo: Define such specific group-names or reduce to flat list?
+def _groupnames(tree):
+  def one_flatten_step(tree):
+    """Helper to flatten tree for one step if it is not a leaf."""
+    return lambda t: tree != t
+
+  def recurse(treedef, tree):
+    """Recursively build the node names. """
+    if tree_util.treedef_is_leaf(treedef):
+      for idx in range(treedef.num_leaves):
+        yield f"/{type(tree).__name__}~{idx}"
+    else:
+      # Get the child treedefs and the node treedef
+      children = [treedef.children(),
+                  tree_util.tree_flatten(
+                    tree, is_leaf=one_flatten_step(tree))[0]]
+      for idx, (child_treedef, child_tree) in enumerate(zip(*children)):
+        for child_name in recurse(child_treedef, child_tree):
+          yield f"/{type(tree).__name__}~{idx}" + child_name
+
+  treedef = tree_util.tree_structure(tree)
+  return list(recurse(treedef, tree))

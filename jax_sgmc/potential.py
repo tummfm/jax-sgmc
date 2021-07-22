Fixed wrong potential due to broadcasting

jax_sgmc/potential.py
@@ -186,6 +186,7 @@ import jax.numpy as jnp
 
 from jax_sgmc import util
 from jax_sgmc.data import CacheState, MiniBatch
+from jax_sgmc.util import host_callback
 
 # Here we define special types
 
@@ -242,7 +243,8 @@ def minibatch_potential(prior: Prior,
     else:
       lks = likelihood(sample, reference_data)
       state = None
-    return lks, state
+    # Ensure that a scalar is returned to avoid broadcasting with mask
+    return jnp.squeeze(lks), state
 
   # Define the strategies to evaluate the likelihoods sequantially, vectorized
   # or in parallel
@@ -291,8 +293,7 @@ def minibatch_potential(prior: Prior,
       stochastic_potential = - N * jnp.mean(batch_likelihoods, axis=0)
       stochastic_variance = jnp.var(batch_likelihoods)
     else:
-      stochastic_potential = - N / n * jnp.sum(
-        jnp.multiply(batch_likelihoods, mask), axis=0)
+      stochastic_potential = - N / n * jnp.dot(batch_likelihoods, mask)
       # Undo scaling when calculating the variance
       sq = jnp.square(batch_likelihoods + stochastic_potential / N)
       stochastic_variance = 1 / (n - 1) * jnp.sum(jnp.multiply(mask, sq))
@@ -365,7 +366,7 @@ def full_potential(prior: Callable[[PyTree], Array],
 
   # Can use the potential evaluation strategy for a minibatch of data. The prior
   # must be evaluated independently.
-  batch_potential = minibatch_potential(lambda sample: 0.0,
+  batch_potential = minibatch_potential(lambda _: 0.0,
                                         likelihood,
                                         strategy=strategy,
                                         has_state=has_state,

tests/test_potential.py
@@ -10,8 +10,8 @@ from jax import random, jit, test_util
 import pytest
 
 # from jax_sgmc.data import mini_batch
-from jax_sgmc.potential import minibatch_potential
-from jax_sgmc.data import mini_batch_information
+from jax_sgmc.potential import minibatch_potential, full_potential
+from jax_sgmc.data import mini_batch_information, NumpyDataLoader, full_reference_data
 
 # Todo: Test the potential evaluation function on arbitrary pytrees.
 
@@ -245,3 +245,51 @@ class TestPotential():
     test_util.check_close(new_state_map, sample)
     test_util.check_close(new_state_vmap, sample)
     test_util.check_close(new_state_pmap, sample)
+
+  @pytest.mark.parametrize("obs, dim, mbsize", itertools.product([7, 11], [3, 5], [2, 3]))
+  def test_full_potential(self, potential, obs, dim, mbsize):
+    prior, likelihood = potential
+    # Setup potential
+
+    scan_pot = minibatch_potential(prior, likelihood, strategy="map")
+
+    # Setup reference data
+    key = random.PRNGKey(0)
+
+    split1, split2 = random.split(key, 2)
+    observations = {"scale": random.exponential(split1, shape=(obs,)),
+                    "power": random.exponential(split2, shape=(obs, dim))}
+    reference_data = observations, mini_batch_information(observation_count=obs,
+                                                          batch_size=obs)
+    sample = {"scale": jnp.array([0.5]), "base": jnp.ones(dim)}
+    init_state = {"scale": jnp.array([0.0]), "base": jnp.zeros(dim)}
+
+    reference_sol, _ = scan_pot(sample, reference_data, state=init_state)
+
+    # Initialize dataloader for full potential evaluation
+    data_loader = NumpyDataLoader(**observations)
+    full_data_map = full_reference_data(data_loader, cached_batches_count=2, mb_size=mbsize)
+
+    map_data_state = full_data_map[0]()
+    vmap_data_state = full_data_map[0]()
+    pmap_data_state = full_data_map[0]()
+
+    map_pot = full_potential(prior, likelihood, strategy="map", full_data_map=full_data_map[1])
+    vmap_pot = full_potential(prior, likelihood, strategy="vmap", full_data_map=full_data_map[1])
+    pmap_pot =  full_potential(prior, likelihood, strategy="pmap", full_data_map=full_data_map[1])
+
+    map_sol, _ = map_pot(sample, map_data_state)
+    vmap_sol, _ = vmap_pot(sample, vmap_data_state)
+    pmap_sol, _ = pmap_pot(sample, pmap_data_state)
+
+    print("Reference solution")
+    print(reference_sol)
+    print("Map solution")
+    print(map_sol)
+    print("Vmap solution")
+    print(vmap_sol)
+    print("Pmap solution")
+    print(pmap_sol)
+
+    assert False
+    test_util.check_close(reference_sol, map_sol)
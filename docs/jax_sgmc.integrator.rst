0081db76e35c20f8560f3d4998d45b99044f8af4
Paul <paul.fuchs@tum.de>
2021-07-26 19:04:11 +0200

    Added standard SGHMC

--- docs/jax_sgmc.integrator.rst
+++ docs/jax_sgmc.integrator.rst
@@ -1,6 +1,28 @@
 jax_sgmc.integrator
 ====================
 
+Overview
+---------
+
 .. automodule:: jax_sgmc.integrator
-   :members:
-   :noindex:
+
+Integrators
+------------
+
+.. autofunction:: jax_sgmc.integrator.obabo
+.. autofunction:: jax_sgmc.integrator.reversible_leapfrog
+.. autofunction:: jax_sgmc.integrator.friction_leapfrog
+.. autofunction:: jax_sgmc.integrator.langevin_diffusion
+
+Integrator States
+------------------
+
+.. autoclass:: jax_sgmc.integrator.ObaboState
+.. autoclass:: jax_sgmc.integrator.LeapfrogState
+.. autoclass:: jax_sgmc.integrator.LangevinState
+
+Utility
+-------
+
+.. autofunction:: jax_sgmc.integrator.random_tree
+.. autofunction:: jax_sgmc.integrator.init_mass
--- jax_sgmc/integrator.py
+++ jax_sgmc/integrator.py
@@ -16,7 +16,7 @@
 
 from collections import namedtuple
 from functools import partial
-from typing import AnyStr, Callable, Any, Tuple, Dict, Union
+from typing import Callable, Any, Tuple, Dict, NamedTuple
 
 import jax.numpy as jnp
 import numpy as onp
@@ -30,67 +30,81 @@ from jax_sgmc.scheduler import schedule
 from jax_sgmc.util import Array, tree_scale, tree_add, tensor_matmul, \
   tree_dot, Tensor
 
-leapfrog_state = namedtuple("leapfrog_state",
-                            ["positions",
-                             "momentum",
-                             "potential",
-                             "model_state",
-                             "data_state",
-                             "key"])
-"""
-Attributes:
-  positions: Latent variables in hamiltonian dynamics formulation
-  momentum: Momentum with the same shape as the latent variables
-  potential: Accumulated energy to calculate MH-correction step
-  model_state: State of the model in the likelihood
-  data_state: State of the random data function
-  key: PRNGKey
-"""
-
-obabo_state = namedtuple("obabo_state",
-                         ["positions",
-                          "momentum",
-                          "potential",
-                          "model_state",
-                          "data_state",
-                          "key",
-                          "kinetic_energy_start",
-                          "kinetic_energy_end"])
-"""
-Attributes:
-  positions: Latent variables in hamiltonian dynamics formulation
-  momentum: Momentum with the same shape as the latent variables
-  potential: Stochastic potential
-  model_state: State of the model in the likelihood
-  data_state: State of the random data function
-  key: PRNGKey
-  kinetic_energy_start: Kinetic energy after the first 1/4-step
-  kinetic_energy_end: Kinetic energy after the last 3/4-step
-"""
-
-langevin_state = namedtuple("langevin_state",
-                            ["latent_variables",
-                             "model_state",
-                             "key",
-                             "adapt_state",
-                             "data_state",
-                             "potential",
-                             "variance"])
-"""State of the langevin diffusion integrator.
-
-Attributes:
-  latent_variables: Current latent variables
-  key: PRNGKey
-  adapt_state: Containing quantities such as momentum for adaption
-  data_state: State of the reference data cache
-  model_state: Variables not considered during inference
-  potential: Stochastic potential from last evaluation
-  variance: Variance of stochastic potential over mini-batch
-"""
-
 PyTree = Any
 
-def init_mass(mass):
+
+class LeapfrogState(NamedTuple):
+  """State of the reversible and friction leapfrog integrator.
+
+  Attributes:
+    positions: Latent variables in hamiltonian dynamics formulation
+    momentum: Momentum with the same shape as the latent variables
+    potential: Accumulated energy to calculate MH-correction step
+    model_state: State of the model in the likelihood
+    data_state: State of the random data function
+    key: PRNGKey
+  """
+  positions: PyTree
+  momentum: PyTree
+  potential: Array
+  model_state: PyTree
+  data_state: CacheState
+  key: Array
+
+
+class ObaboState(NamedTuple):
+  """State of the OBABO integrator.
+
+  Attributes:
+    positions: Latent variables in hamiltonian dynamics formulation
+    momentum: Momentum with the same shape as the latent variables
+    potential: Stochastic potential
+    model_state: State of the model in the likelihood
+    data_state: State of the random data function
+    key: PRNGKey
+    kinetic_energy_start: Kinetic energy after the first 1/4-step
+    kinetic_energy_end: Kinetic energy after the last 3/4-step
+  """
+  positions: PyTree
+  momentum: PyTree
+  potential: Array
+  model_state: PyTree
+  data_state: CacheState
+  key: Array
+  kinetic_energy_start: Array
+  kinetic_energy_end: Array
+
+
+class LangevinState(NamedTuple):
+  """State of the langevin diffusion integrator.
+
+  Attributes:
+    latent_variables: Current latent variables
+    key: PRNGKey
+    adapt_state: Containing quantities such as momentum for adaption
+    data_state: State of the reference data cache
+    model_state: Variables not considered during inference
+    potential: Stochastic potential from last evaluation
+    variance: Variance of stochastic potential over mini-batch
+  """
+  latent_variables: PyTree
+  model_state: PyTree
+  key: Array
+  adapt_state: Any
+  data_state: CacheState
+  potential: Array
+  variance: Array
+
+
+def init_mass(mass) -> MassMatrix:
+  """Initializes a diagonal mass tensor.
+
+  Args:
+    mass: Diagonal mass which has the same tree structure as the sample.
+
+  Returns:
+    Returns a diagonal mass matrix.
+  """
   inv_mass = tree_util.tree_map(
     partial(jnp.power, x2=-1.0),
     mass)
@@ -186,10 +200,11 @@ def obabo(potential_fn: StochasticPotential,
     return tree_add(decayed_momentum, momentum_noise)
 
   # A single OBABO-step of the integrator
-  def _leapfrog_steps(state: obabo_state,
+  def _leapfrog_steps(state: ObaboState,
                       step: jnp.array,
                       mass: PyTree,
                       parameters: schedule = None):
+    del step
     key, split1, split2 = random.split(state.key, num=3)
 
     refreshed_momentum = _momentum_resampling(
@@ -245,7 +260,7 @@ def obabo(potential_fn: StochasticPotential,
     # calculate the acceptance probability for the MH step.
     end_energy = state.kinetic_energy_end + _kinetic_energy(mass, second_updated_momentum)
 
-    new_state = obabo_state(
+    new_state = ObaboState(
       potential=0.5 * (pot_before + pot_after),
       positions=updated_positions,
       momentum=final_refreshed_momentum,
@@ -260,8 +275,7 @@ def obabo(potential_fn: StochasticPotential,
   def init_fn(init_sample: PyTree,
               key: Array = None,
               batch_kwargs: Dict = None,
-              init_model_state: PyTree = None,
-              mass: PyTree = None):
+              init_model_state: PyTree = None):
     """Initializes the initial state of the integrator.
 
     Args:
@@ -288,7 +302,7 @@ def obabo(potential_fn: StochasticPotential,
 
     momentum = tree_util.tree_map(jnp.zeros_like, init_sample)
 
-    init_state = obabo_state(
+    init_state = ObaboState(
       kinetic_energy_start=jnp.array(0.0),
       kinetic_energy_end=jnp.array(0.0),
       potential=jnp.array(0.0),
@@ -301,10 +315,10 @@ def obabo(potential_fn: StochasticPotential,
     return init_state
 
   @partial(named_call, name='obabo_integration')
-  def integrate(state: obabo_state,
+  def integrate(state: ObaboState,
                 parameters: schedule,
                 mass: PyTree = None
-                ) -> obabo_state:
+                ) -> ObaboState:
 
     # If the mass is not adapted, take the constant mass if provided
     if const_mass is None:
@@ -323,7 +337,7 @@ def obabo(potential_fn: StochasticPotential,
 
     return state
 
-  def get_fn(state: leapfrog_state):
+  def get_fn(state: LeapfrogState):
     """Returns the latent variables."""
     return {"variables": state.positions,
             "energy": state.potential}
@@ -384,7 +398,7 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
     unscaled_energy = tree_dot(momentum_sum, scaled_gradient)
     return scale * unscaled_energy
 
-  def _body_fun(state: leapfrog_state,
+  def _body_fun(state: LeapfrogState,
                 step: jnp.array,
                 parameters: schedule,
                 mass: MassMatrix):
@@ -432,7 +446,7 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
       0.5 * parameters.step_size)
     accumulated_energy = energy + state.potential
 
-    new_state = leapfrog_state(
+    new_state = LeapfrogState(
       potential=accumulated_energy,
       key=key,
       positions=positions,
@@ -481,7 +495,7 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
     key, split = random.split(key)
     momentum = _cov_scaled_noise(split, mass, init_sample)
 
-    init_state = leapfrog_state(
+    init_state = LeapfrogState(
       potential=jnp.array(0.0),
       key=key,
       positions=init_sample,
@@ -492,10 +506,10 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
     return init_state
 
   @partial(named_call, name='leapfrog_integration')
-  def integrate(state: leapfrog_state,
+  def integrate(state: LeapfrogState,
                 parameters: schedule,
                 mass: PyTree = None
-                ) -> leapfrog_state:
+                ) -> LeapfrogState:
 
     # Use default values if mass matrix not provided
     if not mass:
@@ -504,24 +518,15 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
       else:
         mass = init_mass(tree_util.tree_map(jnp.ones_like, state.positions))
 
-    # # Resample momentum to make process reversible (otherwise skew-reversible)
-    key, split = random.split(state.key)
-    # momentum = _cov_scaled_noise(split, mass, state.momentum)
-    momentum = state.momentum
-
-    # Change direction if last step has been rejected (only if momentum is not
-    # resampled)
-    # momentum = tree_scale(direction, state.momentum)
-
     # Half step for leapfrog integration
     positions = _position_update(
-      0.5 * parameters.step_size, mass, state.positions, momentum)
+      0.5 * parameters.step_size, mass, state.positions, state.momentum)
 
     # Do the leapfrog steps
-    state = leapfrog_state(
+    state = LeapfrogState(
       positions=positions,
-      momentum=momentum,
-      key=key,
+      momentum=state.momentum,
+      key=state.key,
       potential=jnp.array(0.0),
       model_state=state.model_state,
       data_state=state.data_state)
@@ -536,7 +541,7 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
     positions = _position_update(
       0.5 * parameters.step_size, mass, state.positions, state.momentum)
 
-    final_state = leapfrog_state(
+    final_state = LeapfrogState(
       positions=positions,
       momentum=state.momentum,
       key=state.key,
@@ -546,28 +551,32 @@ def reversible_leapfrog(potential_fn: StochasticPotential,
 
     return final_state
 
-  def get_fn(state: leapfrog_state):
-    """Returns the latent variables."""
-    # Todo: This is not truly the likelihood
+  def get_fn(state: LeapfrogState):
     return {"variables": state.positions,
             "energy": state.potential}
 
   return init_fn, integrate, get_fn
 
 
-def friction_leapfrog(key: Array,
-                      T: Array,
-                      data: CacheState,
-                      potential_strategy: AnyStr='map'
-                      ) -> Callable[[leapfrog_state], leapfrog_state]:
+def friction_leapfrog(potential_fn: StochasticPotential,
+                      batch_fn: RandomBatch,
+                      steps: int = 10,
+                      friction: Array = 0.25,
+                      const_mass: PyTree = None
+                      ) -> Tuple[Callable, Callable, Callable]:
   """Initializes the original SGHMC leapfrog integrator.
 
+  Original SGHMC from [1].
+
+  [1] https://arxiv.org/pdf/1402.4102.pdf
+
   Args:
-    key: PRNGKey to generate noise
-    T: Number of leapfrog steps until acceptance step is run
-    data: Reference data object to draw minibatches while integrating.
-    initial_state: Usually the last state of the previous integration run.
-    potential_strategy: See potential module
+    potential_fn: Likelihood and prior applied over a minibatch of data
+    batch_fn: Function to draw a mini-batch of reference data
+    steps: Number of intermediate leapfrog steps
+    friction: Decay of momentum to counteract induced noise due to stochastic
+     gradients
+    const_mass: Mass matrix of the hamiltonian process
 
   Returns:
     Returns a function running the non conservative leapfrog integrator for T
@@ -575,24 +584,135 @@ def friction_leapfrog(key: Array,
 
   """
 
-  # We need to initialize the potential module to evaluate the potential for a
-  # minibatch of data.
+  init_data, get_data = batch_fn
+  stochastic_gradient = value_and_grad(potential_fn, has_aux=True)
 
-  # Should there be an init function and should we pass back initial states or
-  # are they obvious?
+  # Calculate the inverse and the square root
+  if const_mass:
+    const_mass = init_mass(const_mass)
+  else:
+    const_mass = None
 
-  def integrate(leapfrog_state: leapfrog_state) -> leapfrog_state:
-    # Here it could be possible to evaluate multiple chains at once. The overal
-    # method is not jit-able, so we cannot move out the parallelization /
-    # vectorization on a higher level.
+  def _body_fun(state: LeapfrogState,
+                step: jnp.array,
+                parameters: schedule,
+                mass: MassMatrix):
+    del step
+    scaled_momentum = tensor_matmul(mass.inv, state.momentum)
+    position_update = tree_scale(parameters.step_size, scaled_momentum)
+    new_positions = tree_add(state.positions, position_update)
 
-    pass
+    data_state, mini_batch = get_data(state.data_state, information=True)
+    (pot, model_state), gradient = stochastic_gradient(
+      new_positions,
+      mini_batch,
+      state=state.model_state)
+    scaled_gradient = tree_scale(-parameters.step_size, gradient)
+    decayed_momentum = tree_scale(
+      -parameters.step_size * friction,
+      scaled_momentum)
 
-  return integrate
+    key, split = random.split(state.key)
+    noise = random_tree(split, decayed_momentum)
+    scaled_noise = tree_scale(
+      jnp.sqrt(2 * friction * parameters.step_size),
+      noise)
 
-  # Todo: Find a general method to deal with direct hessian inversion (e. g.
-  #   fisher scoring) or iterative inversion. -> Two different solvers
-  #   For langevin diffusion is might be possible to implement an adaption step.
+    new_momentum = tree_add(
+      tree_add(decayed_momentum, scaled_gradient),
+      scaled_noise)
+
+    new_state = LeapfrogState(
+      potential=pot,
+      key=key,
+      positions=new_positions,
+      momentum=new_momentum,
+      data_state=data_state,
+      model_state=model_state)
+
+    return new_state, None
+
+  def init_fn(init_sample: PyTree,
+              key: Array = None,
+              batch_kwargs: Dict = None,
+              init_model_state: PyTree = None):
+    """Initializes the initial state of the integrator.
+
+    Args:
+      init_sample: Initial latent variables
+      key: Initial PRNGKey
+      batch_kwargs: Determine the initial state of the random data chain
+      init_model_state: State of the model.
+
+    Returns:
+      Returns the initial state of the integrator.
+
+    """
+
+    # Initializing the initial state here makes it easier to add additional
+    # variables which might be only necessary in special case
+    if batch_kwargs is None:
+      batch_kwargs = {}
+
+    reference_data_state = init_data(**batch_kwargs)
+
+    # Only shape of momentum is important, as momentum is resampled in each
+    # integration step
+    if key is None:
+      key = random.PRNGKey(0)
+
+    momentum = init_sample
+
+    init_state = LeapfrogState(
+      potential=jnp.array(0.0),
+      key=key,
+      positions=init_sample,
+      momentum=momentum,
+      data_state=reference_data_state,
+      model_state=init_model_state)
+
+    return init_state
+
+  @partial(named_call, name='leapfrog_integration')
+  def integrate(state: LeapfrogState,
+                parameters: schedule,
+                mass: PyTree = None
+                ) -> LeapfrogState:
+
+    # Use default values if mass matrix not provided
+    if not mass:
+      if const_mass:
+        mass = const_mass
+      else:
+        mass = init_mass(tree_util.tree_map(jnp.ones_like, state.positions))
+
+    # Resample momentum
+    key, split = random.split(state.key)
+    noise = random_tree(split, state.momentum)
+    momentum = tensor_matmul(mass.sqrt, noise)
+
+    # Do the leapfrog steps
+    state = LeapfrogState(
+      positions=state.positions,
+      momentum=momentum,
+      key=key,
+      potential=jnp.array(0.0),
+      model_state=state.model_state,
+      data_state=state.data_state)
+    final_state, _ = lax.scan(
+      partial(_body_fun, parameters=parameters, mass=mass),
+      state,
+      onp.arange(steps))
+
+    return final_state
+
+  def get_fn(state: LeapfrogState):
+    """Returns the latent variables."""
+    # Todo: This is not truly the likelihood
+    return {"variables": state.positions,
+            "energy": state.potential}
+
+  return init_fn, integrate, get_fn
 
 def langevin_diffusion(
         potential_fn: StochasticPotential,
@@ -663,7 +783,7 @@ def langevin_diffusion(
 
     reference_data_state = batch_init(**batch_kwargs)
 
-    init_state = langevin_state(
+    init_state = LangevinState(
       key=key,
       latent_variables=init_sample,
       adapt_state=adaption_state,
@@ -678,7 +798,7 @@ def langevin_diffusion(
   # Returns the important parameters of a state and excludes. Makes information
   # hiding possible
 
-  def get_fn(state: langevin_state):
+  def get_fn(state: LangevinState):
     """Returns the latent variables."""
     return {"variables": state.latent_variables,
             "likelihood": -state.potential}
@@ -686,7 +806,7 @@ def langevin_diffusion(
   # Update according to the integrator update rule
 
   @partial(named_call, name='langevin_diffusion_step')
-  def update_fn(state: langevin_state,
+  def update_fn(state: LangevinState,
                 parameters: schedule):
     """Updates the integrator state according to a schedule.
 
@@ -738,7 +858,7 @@ def langevin_diffusion(
 
     # Conclude the variable update by adding the step to the current samples
     new_sample = tree_add(state.latent_variables, update_step)
-    new_state = langevin_state(
+    new_state = LangevinState(
       key=key,
       latent_variables=new_sample,
       adapt_state=adapt_state,

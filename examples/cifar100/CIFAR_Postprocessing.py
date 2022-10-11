b7a7cce191b03a3aff6bc6498a9e3476e9cd0142
Ana Cukarska <ana.cukarska@tum.de>
2022-10-11 12:05:10 +0200

    working postprocessing with train-val-test split

--- examples/cifar100/CIFAR_Postprocessing.py
+++ examples/cifar100/CIFAR_Postprocessing.py
@@ -1,4 +1,6 @@
 import sys
+
+import h5py
 import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str('1')  # needs to stay before importing jax
@@ -21,7 +23,7 @@ cached_batches = 1
 num_classes = 10
 weight_decay = 5.e-4
 
-(train_images, train_labels), _ = \
+(train_images, train_labels), (test_images, test_labels) = \
     tf.keras.datasets.cifar10.load_data()
 
 import tensorflow_datasets as tfds
@@ -29,17 +31,15 @@ from jax_sgmc import data
 from jax_sgmc.data.tensorflow_loader import TensorflowDataLoader
 
 train_dataset, train_info = tfds.load('Cifar10',
-                                      split='train',
-                                      with_info=True)
-train_loader = TensorflowDataLoader(train_dataset,
-                                    shuffle_cache=1000,
-                                    exclude_keys=['id'])
+                                      split=['train[:70%]', 'test[70%:]'],
+                                      with_info=True)  # shuffling is disabled by default
+train_dataset, validation_dataset = train_dataset
 
-train_batch_fn = data.random_reference_data(train_loader, cached_batches, batch_size)
+validation_images, validation_labels = train_images[-10000:, :, :, :], train_labels[-10000:, :]
+from jax_sgmc.data.numpy_loader import DeviceNumpyDataLoader
 
-batch_init, batch_get, batch_release = train_batch_fn
-_, batch_data = batch_get(batch_init(), information=True)
-init_batch, info_batch = batch_data
+validation_loader = DeviceNumpyDataLoader(image=validation_images, label=validation_labels)
+
 
 def init_resnet():
     @hk.transform_with_state
@@ -48,15 +48,68 @@ def init_resnet():
         resnet50 = hk.nets.ResNet50(num_classes)
         logits = resnet50(images, is_training=is_training)
         return logits
+
     return resnet.init, resnet.apply
 
-init, _ = init_resnet()
-init_params, _ = init(random.PRNGKey(0), init_batch)
 
+validation_batch_fn = data.random_reference_data(validation_loader, cached_batches, batch_size)
+
+batch_init, batch_get, batch_release = validation_batch_fn
+zeros_init_batch = validation_loader.initializer_batch(batch_size)
+_, batch_data = batch_get(batch_init(), information=True)
+init_batch, info_batch = batch_data
+init, apply_resnet = init_resnet()
+init_params, init_resnet_state = init(random.PRNGKey(0), init_batch)
 sample = {"w": init_params}
 
+# from jax_sgmc.data.tensorflow_loader import TensorflowDataLoader
 
-filename = '/home/student/ana/jax-sgmc/examples/cifar100/results'
-custom_loader = HDF5Loader(filename, '', sample)
+# validation_loader = TensorflowDataLoader(validation_dataset,
+#                                          shuffle_cache=1000,
+#                                          exclude_keys=['id'])
 
 
+val_batch_init, val_batch_get, val_release = data.random_reference_data(validation_loader, cached_batches, batch_size)
+
+temp = val_batch_init(shuffle=True, in_epochs=True)
+first_batch_state, first_batch_data = val_batch_get(temp, information=True)
+first_batch, first_info_batch = first_batch_data
+
+
+def map_fn(batch, mask, count):
+    params = batch["w"]
+    params = tree_map(lambda x: jnp.array(x[0]), params)
+    logits, _ = apply_resnet(params, init_resnet_state, None, first_batch)
+    mini_batch_state = first_batch_state
+    for i in range(validation_loader.static_information['observation_count'] // batch_size):
+        mini_batch_state, mini_batch = val_batch_get(mini_batch_state)
+        logits = jnp.concatenate([logits, apply_resnet(params, init_resnet_state, None, mini_batch)[0]], axis=0)
+    return logits, count + 1
+
+
+with h5py.File('/home/student/ana/jax-sgmc/examples/cifar100/results', "r") as file:
+    postprocess_loader = HDF5Loader('/home/student/ana/jax-sgmc/examples/cifar100/results', subdir="/chain~0/variables",
+                                    sample=sample)
+
+    my_parameter_mapper, _ = data.full_data_mapper(postprocess_loader, 1, 1)
+    result, _ = my_parameter_mapper(map_fn, 0, masking=True)
+
+    i = 0
+    # my_full_data_mapper, release = data.full_data_mapper(postprocess_loader, cached_batches_count=1, mb_size=1)
+
+    # logits, _ = my_full_data_mapper(apply_resnet, (init_resnet_state, None, init_batch))
+
+exit()
+from jax_sgmc.data.numpy_loader import DeviceNumpyDataLoader
+
+test_loader = DeviceNumpyDataLoader(image=test_images, label=test_labels)
+
+test_batch_init, test_batch_get, test_release = data.random_reference_data(test_loader, cached_batches, batch_size)
+
+# _, test_init_batch = test_batch_get(test_batch_init(), information=True)
+
+
+# actually getting the first batch
+temp = test_batch_init(shuffle=True, in_epochs=True)
+first_batch_state, first_batch_data = test_batch_get(temp, information=True)
+first_batch, first_info_batch = first_batch_data

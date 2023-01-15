02942737d8d77c285851c4ef9e86f9665e89da5e
Ana Cukarska <ana.cukarska@tum.de>
2023-01-15 18:29:07 +0100

    training with MobileNet

---
+++ examples/cifar100/CIFAR10_MobileNetV1.py
@@ -0,0 +1,175 @@
+import contextlib
+import logging
+import time
+import timeit
+
+import functools
+import os
+
+from typing import Iterable, Mapping, NamedTuple, Tuple
+
+import tree
+
+os.environ["CUDA_VISIBLE_DEVICES"] = str('0')  # needs to stay before importing jax
+
+from jax import jit, nn, pmap, grad, tree_util, tree_leaves, tree_flatten, tree_map, lax, random, numpy as jnp, \
+    scipy as jscipy
+from jax_sgmc import data, potential, adaption, scheduler, integrator, solver, io, alias
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import haiku as hk
+import numpy as onp
+import optax
+import jax
+import jmp
+import tree_math
+import h5py
+from jax_sgmc.data.tensorflow_loader import TensorflowDataLoader
+from jax_sgmc.data.numpy_loader import DeviceNumpyDataLoader
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# if len(sys.argv) > 1:
+#     visible_device = str(sys.argv[1])
+# else:
+#     visible_device = 3
+# os.environ["CUDA_VISIBLE_DEVICES"] = str(visible_device)  # controls on which gpu the program runs
+
+## Configuration parameters
+batch_size = 128
+cached_batches = 1
+num_classes = 10
+weight_decay = 5.e-4
+deterministic_weight_decay = 1e-4
+train_smoothing = 0.1
+eval_batch_size = 1000
+train_epochs = 90
+
+## Load dataset
+(train_images, train_labels), (test_images, test_labels) = \
+    tf.keras.datasets.cifar10.load_data()
+
+# Use tensorflow dataset directly. The 'id' must be excluded as text is not
+# supported by jax
+train_dataset, train_info = tfds.load('Cifar10',
+                                      split=['train[:70%]', 'test[70%:]'],
+                                      with_info=True)
+train_dataset, validation_dataset = train_dataset
+train_loader = TensorflowDataLoader(train_dataset,
+                                    shuffle_cache=1000,
+                                    exclude_keys=['id'])
+
+# ana: test data not needed here, but keeping the code nonetheless
+test_loader = DeviceNumpyDataLoader(image=test_images, label=test_labels)
+train_batch_fn = data.random_reference_data(train_loader, cached_batches, batch_size)
+test_batch_init, test_batch_get, test_release = data.core.random_reference_data(test_loader, cached_batches, batch_size)
+
+# get first batch to init NN
+# TODO: Maybe write convenience function for this common usecase?
+batch_init, batch_get, batch_release = train_batch_fn
+
+zeros_init_batch = train_loader.initializer_batch(batch_size)  # ana: it doesn't help if I change this to ones
+batch_state, batch_data = batch_get(batch_init(), information=True)
+init_batch, info_batch = batch_data
+test_init_state, test_init_batch = test_batch_get(test_batch_init(), information=True)
+
+
+# MobileNet Model
+def init_mobilenet():
+    @hk.transform
+    def mobilenetv1(batch, is_training=True):
+        images = batch["image"].astype(jnp.float32) / 255.
+        mobilenet = hk.nets.MobileNetV1(num_classes=num_classes, use_bn=False)
+        logits = mobilenet(images, is_training=True)
+        return logits
+
+    return mobilenetv1.init, mobilenetv1.apply
+
+
+init, apply_mobilenet = init_mobilenet()
+init_params = init(random.PRNGKey(0), init_batch)
+
+# sanity-check prediction
+logits = apply_mobilenet(init_params, None, init_batch)
+
+
+# Initialize potential with log-likelihood
+# TODO logits are zeros everywhere, this does not seem correct, because input
+#  batch is a proper image. Make sure this is not the case any more  after
+#  switching to MobileNet
+
+# TODO the likelihood had to process a batch of data before due to batchnorm.
+#  Without batchnorm, we could also go the standard way and only run the model
+#  for a single input, and let jax-sgmc deal with the batching.
+def likelihood(sample, observations):
+    logits = apply_mobilenet(sample["w"], None, observations)
+    # log-likelihood is negative cross entropy
+    log_likelihood = -optax.softmax_cross_entropy_with_integer_labels(
+        logits, observations["label"])
+    return log_likelihood
+
+prior_scale = 1.
+def gaussian_prior(sample):
+    params = tree_math.Vector(sample["w"])
+    log_pdf = tree_math.unwrap(jscipy.stats.norm.logpdf, vector_argnums=0)(
+        params, loc=0., scale=prior_scale)
+    return log_pdf.sum()
+
+
+def prior(sample):
+    return jnp.array(1.0, dtype=jnp.float32)
+
+
+# The likelihood accepts a batch of data, so no batching strategy is required, instead, is_batched must be set to true.
+# The likelihood signature changes from:   (Sample, Data) -> Likelihood
+#                                   to :   (State, Sample, Data) -> Likelihood, NewState
+# if has_state is set to true.
+potential_fn = potential.minibatch_potential(prior=prior,
+                                             likelihood=likelihood,
+                                             is_batched=True,
+                                             strategy='vmap')  # or change to pmap
+sample = {"w": init_params, "std": jnp.array([1.0])}
+_, returned_likelihoods = potential_fn(sample, batch_data, likelihoods=True)
+
+
+
+# Setup Integrator
+# Number of iterations: Ca. 0.035 seconds per iteration (including saving)
+iterations = 10000
+
+rms_prop = adaption.rms_prop()
+rms_integrator = integrator.langevin_diffusion(potential_fn,
+                                               train_batch_fn,
+                                               rms_prop)
+
+# Schedulers
+rms_step_size = scheduler.polynomial_step_size_first_last(first=5e-7,
+                                                          # a good starting point is 1e-3, start sampling at 1e-6
+                                                          last=5e-8)
+
+burn_in = scheduler.initial_burn_in(
+    5000)  # large burn-in: if you need 100k for deterministic training, then 200k burn-in
+
+# Has ca. 23.000.000 parameters, so not more than 500 samples fit into RAM
+rms_random_thinning = scheduler.random_thinning(rms_step_size, burn_in, 50)
+
+rms_scheduler = scheduler.init_scheduler(step_size=rms_step_size,
+                                         burn_in=burn_in,
+                                         thinning=rms_random_thinning)
+
+with h5py.File('mobilenet_50_samples', "w") as file:
+    data_collector = io.HDF5Collector(file)
+    saving = io.save(data_collector)
+
+    rms_sgld = solver.sgmc(rms_integrator)
+    rms_run = solver.mcmc(rms_sgld,
+                          rms_scheduler,
+                          saving=saving)
+    rms_integ = rms_integrator[0](sample)
+
+    start = time.time()
+    rms_results = rms_run(rms_integ,
+                          iterations=iterations)
+
+print("Finished")
--- examples/cifar100/CIFAR_Postprocessing.py
+++ examples/cifar100/CIFAR_Postprocessing.py
@@ -6,12 +6,12 @@ import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str('1')  # needs to stay before importing jax
 
-from jax import nn, tree_leaves, random, numpy as jnp
+from jax import nn, tree_leaves, random
 import tensorflow as tf
 import tensorflow_datasets
 import haiku as hk
 import numpy as onp
-from jax import lax, tree_map, tree_leaves, numpy as jnp
+from jax import lax, tree_map, tree_leaves, tree_flatten, numpy as jnp
 
 from jax_sgmc.data.hdf5_loader import HDF5Loader
 
@@ -40,27 +40,34 @@ validation_images, validation_labels = train_images[-10000:, :, :, :], train_lab
 from jax_sgmc.data.numpy_loader import DeviceNumpyDataLoader
 
 validation_loader = DeviceNumpyDataLoader(image=validation_images, label=validation_labels)
+training_loader = DeviceNumpyDataLoader(image=train_images[:-10000, :, :, :], label=train_labels[:-10000, :])
+
 
 
 def init_resnet():
-    @hk.transform_with_state
-    def resnet(batch, is_training=True):
+    @hk.transform
+    def mobilenetv1(batch, is_training=True):
         images = batch["image"].astype(jnp.float32) / 255.
-        resnet50 = hk.nets.ResNet50(num_classes)
-        logits = resnet50(images, is_training=is_training)
+        # resnet50 = hk.nets.ResNet50(num_classes)
+        mobilenet = hk.nets.MobileNetV1(num_classes=num_classes, use_bn=False)
+        # logits = resnet50(images, is_training=is_training)
+        logits = mobilenet(images, is_training=True)
         return logits
 
-    return resnet.init, resnet.apply
+    return mobilenetv1.init, mobilenetv1.apply
 
 
 validation_batch_fn = data.random_reference_data(validation_loader, cached_batches, batch_size)
+train_batch_fn = data.random_reference_data(training_loader, cached_batches, batch_size)
 
-batch_init, batch_get, batch_release = validation_batch_fn
-zeros_init_batch = validation_loader.initializer_batch(batch_size)
+# batch_init, batch_get, batch_release = validation_batch_fn
+batch_init, batch_get, batch_release = train_batch_fn
+# zeros_init_batch = validation_loader.initializer_batch(batch_size)
+zeros_init_batch = training_loader.initializer_batch(batch_size)
 _, batch_data = batch_get(batch_init(), information=True)
 init_batch, info_batch = batch_data
 init, apply_resnet = init_resnet()
-init_params, init_resnet_state = init(random.PRNGKey(0), init_batch)
+init_params = init(random.PRNGKey(0), init_batch)
 sample = {"w": init_params}
 
 # from jax_sgmc.data.tensorflow_loader import TensorflowDataLoader
@@ -70,10 +77,13 @@ sample = {"w": init_params}
 #                                          exclude_keys=['id'])
 
 
-val_batch_init, val_batch_get, val_release = data.random_reference_data(validation_loader, cached_batches, batch_size)
+# val_batch_init, val_batch_get, val_release = data.random_reference_data(validation_loader, cached_batches, batch_size)
+train_batch_init, train_batch_get, train_release = data.random_reference_data(training_loader, cached_batches, batch_size)
 
-temp = val_batch_init(shuffle=False, in_epochs=True)
-first_batch_state, first_batch_data = val_batch_get(temp, information=True)
+# temp = val_batch_init(shuffle=False, in_epochs=True)
+# first_batch_state, first_batch_data = val_batch_get(temp, information=True)
+temp = train_batch_init(shuffle=False, in_epochs=True)
+first_batch_state, first_batch_data = train_batch_get(temp, information=True)
 first_batch, first_info_batch = first_batch_data
 target_labels = np.empty([0, 0])
 
@@ -81,57 +91,56 @@ target_labels = np.empty([0, 0])
 def map_fn(batch, mask, carry):
     params = batch["w"]
     params = tree_map(lambda x: jnp.array(x[0]), params)
-    logits, _ = apply_resnet(params, init_resnet_state, None, first_batch)
+    logits = apply_resnet(params, None, first_batch)
     target_labels = jnp.array(first_batch['label'])
     mini_batch_state = first_batch_state
     # for i in range(validation_loader.static_information['observation_count'] // batch_size):
     for i in range(6):
-        mini_batch_state, mini_batch = val_batch_get(mini_batch_state)
+        mini_batch_state, mini_batch = train_batch_get(mini_batch_state)
         target_labels = jnp.concatenate([target_labels, jnp.array(mini_batch['label'])], axis=0)
-        logits = jnp.concatenate([logits, apply_resnet(params, init_resnet_state, None, mini_batch)[0]], axis=0)
+        logits = jnp.concatenate([logits, apply_resnet(params, None, mini_batch)], axis=0)
     return [logits, target_labels], carry + 1
 
+def view_samples_fn(batch, mask, carry):
+    params = batch["w"]
+    params = tree_flatten(params)
+    samples = params[0]
+    return samples, carry + 1
 
-with h5py.File('/home/student/ana/jax-sgmc/examples/cifar100/results_small', "r") as file:
+
+with h5py.File('/home/student/ana/jax-sgmc/examples/cifar100/mobilenet_50_samples', "r") as file:
     postprocess_loader = HDF5Loader(
-        '/home/student/ana/jax-sgmc/examples/cifar100/results_small',
+        '/home/student/ana/jax-sgmc/examples/cifar100/mobilenet_50_samples',
         subdir="/chain~0/variables",
         sample=sample)
 
     my_parameter_mapper, _ = data.full_data_mapper(postprocess_loader, 1, 1)
     out, _ = my_parameter_mapper(map_fn, 0, masking=True)
+    # weight_samples, _ = my_parameter_mapper(view_samples_fn, 0, masking=True) # not mapping correctly; obtaining only one set of weights here
+    # exit()
     result, target = out
     target_labels_array = onp.array(jnp.mean(target, axis=0))
-    import scipy
     import matplotlib.pyplot as plt
 
     results_np_array = onp.array(result[:, 1, :])
 
     accuracy = []
+
+    correct_count = 0
+    incorrect_count = 0
     for i in range(result.shape[0]):
-        for j in range(result.shape[1]):
-            correct_count = 0
-            incorrect_count = 0
-            sliced_results = (onp.array(result[:, j, :]))
-            argmax_results = onp.argmax(sliced_results, axis=-1)
-            argmax_probabilities = scipy.special.softmax(sliced_results)
-            mean = onp.mean(result[:, j, :], axis=0)
-            correct_count += onp.sum(argmax_results==target_labels_array[i])
-            incorrect_count += onp.sum(argmax_results!=target_labels_array[i])
-            # plt.hist(argmax_results, bins=np.arange(0, 10, 1))
-            # plt.show()
+        sliced_results = (onp.array(result[:i+1, :, :]))
+        argmax_results = onp.argmax(sliced_results, axis=-1)
+        for j in range(argmax_results.shape[1]):
+            correct_count += argmax_results[i][j]==int(target_labels_array[j])
+            incorrect_count += argmax_results[i][j]!=int(target_labels_array[j])
         accuracy.append(correct_count / (correct_count + incorrect_count))
 
     plt.plot(accuracy)
     plt.xlabel("num of sampled params")
     plt.ylabel("accuracy")
     plt.show()
-    # print("correct predictions: ", correct_count)
-    # print("incorrect predictions: ", incorrect_count)
-    # print("accuracy: ", correct_count/(correct_count+incorrect_count))
-    # my_full_data_mapper, release = data.full_data_mapper(postprocess_loader, cached_batches_count=1, mb_size=1)
-
-    # logits, _ = my_full_data_mapper(apply_resnet, (init_resnet_state, None, init_batch))
+    plt.savefig("accuracy_plot.png")
 
 exit()
 from jax_sgmc.data.numpy_loader import DeviceNumpyDataLoader

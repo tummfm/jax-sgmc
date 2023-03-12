1b52a7ef000dc9b59d95f8c5456bd0679a71366d
Ana Cukarska <ana.cukarska@tum.de>
2023-03-12 12:09:05 +0100

    changed normalization

--- examples/cifar100/CIFAR10_MobileNetV1.py
+++ examples/cifar100/CIFAR10_MobileNetV1.py
@@ -35,10 +35,10 @@ num_classes = 10
 weight_decay = 5.e-4
 
 # parameters
-iterations = int(1e+6)
-batch_size = 64
-burn_in_size = int(5e+5)
-lr_first = 1e-4
+iterations = int(1e+5)
+batch_size = 1024
+burn_in_size = int(5e+3)
+lr_first = 0.001
 lr_last = 5e-8
 temperature_param = 0.01
 accepted_samples = 40
@@ -52,9 +52,9 @@ CIFAR10_STD = jnp.array([0.2023, 0.1994, 0.2010])
 
 import numpy as np
 
-train_images = np.true_divide(train_images, 255, dtype=np.float32)
-train_mean = np.mean(train_images, axis=(0, 1, 2))
-train_std = np.std(train_images, axis=(0, 1, 2))
+# train_images = np.true_divide(train_images, 255, dtype=np.float32)
+train_mean = np.mean(np.true_divide(train_images, 255, dtype=np.float32), axis=(0, 1, 2))
+train_std = np.std(np.true_divide(train_images, 255, dtype=np.float32), axis=(0, 1, 2))
 
 # Use tensorflow dataset directly. The 'id' must be excluded as text is not
 # supported by jax
@@ -62,9 +62,10 @@ dataset, train_info = tfds.load('Cifar10',
                                 split=['train[:70%]', 'test[70%:]'],
                                 with_info=True)
 train_dataset, test_dataset = dataset
-train_loader = TensorflowDataLoader(train_dataset,
-                                    shuffle_cache=1000,
-                                    exclude_keys=['id'])
+train_loader = NumpyDataLoader(image=train_images, label=np.squeeze(train_labels))
+# train_loader = TensorflowDataLoader(train_dataset,
+#                                     shuffle_cache=1000,
+#                                     exclude_keys=['id'])
 train_batch_fn = data.random_reference_data(train_loader, cached_batches,
                                             batch_size)
 
@@ -91,6 +92,7 @@ def init_mobilenet():
     def mobilenetv1(batch, is_training=True):
         # images = batch["image"].astype(jnp.float32) / 255.
         images =  jnp.true_divide((batch["image"].astype(jnp.float32) - train_mean), train_std)
+        # images =  batch["image"].astype(jnp.float32)
         mobilenet = hk.nets.MobileNetV1(num_classes=num_classes, use_bn=False)
         logits = mobilenet(images, is_training=True)
         return logits
@@ -99,7 +101,7 @@ def init_mobilenet():
 
 
 init, apply_mobilenet = init_mobilenet()
-apply_mobilenet = jit(apply_mobilenet)
+# apply_mobilenet = jit(apply_mobilenet)
 init_params = init(random.PRNGKey(0), init_batch)
 
 # sanity-check prediction
@@ -121,7 +123,7 @@ if max_likelihood:
     def loss_fn(params, batch):
         logits = apply_mobilenet(params, None, batch)
         cross_entropies = optax.softmax_cross_entropy_with_integer_labels(
-            logits, batch["label"])
+            logits, jnp.squeeze(batch["label"]))
         return jnp.mean(cross_entropies)
 
 
@@ -149,6 +151,40 @@ if max_likelihood:
     plt.plot(loss_list)
     plt.savefig('mobilenet_adam_1.png')
     plt.show()
+    from jax_sgmc.data.numpy_loader import DeviceNumpyDataLoader
+
+    training_loader = DeviceNumpyDataLoader(image=train_images[:-10000, :, :, :], label=train_labels[:-10000, :])
+    train_batch_fn = data.random_reference_data(training_loader, cached_batches, batch_size)
+    batch_init, batch_get, batch_release = train_batch_fn
+    # zeros_init_batch = validation_loader.initializer_batch(batch_size)
+    zeros_init_batch = training_loader.initializer_batch(batch_size)
+    _, batch_data = batch_get(batch_init(), information=True)
+    init_batch, info_batch = batch_data
+    init, apply_resnet = init_mobilenet()
+    init_params = init(random.PRNGKey(0), init_batch)
+    sample = {"w": init_params}
+    train_batch_init, train_batch_get, train_release = data.random_reference_data(training_loader, cached_batches,
+                                                                                  batch_size)
+    temp = train_batch_init(shuffle=False, in_epochs=True)
+    first_batch_state, first_batch_data = train_batch_get(temp, information=True)
+    first_batch, first_info_batch = first_batch_data
+    logits = apply_mobilenet(params, None, init_batch)
+    target_labels = onp.array(init_batch['label'])
+    mini_batch_state = first_batch_state
+    accuracy = []
+    correct_count = 0
+    incorrect_count = 0
+    for i in range(train_images.shape[0] // batch_size):  # go over minibatch of training data
+        mini_batch_state, mini_batch = train_batch_get(mini_batch_state, information=False)
+        temp_labels = onp.array(mini_batch['label'])
+        # target_labels = onp.concatenate([target_labels, onp.array(mini_batch['label'])], axis=0)
+        temp_logits = apply_mobilenet(params, None, mini_batch)
+        # logits = onp.concatenate([logits, temp_logits], axis=0)
+        argmax_results = onp.argmax(temp_logits, axis=-1)
+        correct_count += sum(argmax_results == onp.squeeze(temp_labels))
+        incorrect_count += sum(argmax_results != onp.squeeze(temp_labels))
+    accuracy.append(np.true_divide(correct_count, (correct_count + incorrect_count)))
+    print(accuracy)
     sys.exit()
 
 prior_scale = 1.
@@ -176,6 +212,7 @@ potential_fn = potential.minibatch_potential(prior=prior,
                                              temperature=temperature_param)
 
 sample = {"w": init_params, "std": jnp.array([1.0])}
+# batch_data[0]['label'] = jnp.squeeze(batch_data[0]['label'])
 _, returned_likelihoods = potential_fn(sample, batch_data, likelihoods=True)
 
 use_alias = True
@@ -257,7 +294,7 @@ if use_alias:
     plt.plot(onp.arange(1, len(accuracy) + 1, step=1), accuracy)
     plt.xlabel("num of sampled params")
     plt.ylabel("accuracy")
-    plt.savefig("accuracy_plot_mobilenet_3.png")
+    plt.savefig("accuracy_plot_mobilenet_100000iters.png")
     plt.show()
     exit()
 
